f7d0df7cfc3b61125729755fab5f92df4cdb0ff1
Orhan <orhan.dogan@tatilbudur.com>
2016-11-07 09:34:57 +0200

    alp

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -5,5 +5,6 @@ public class JavaHelloWorld
         System.out.println("Java Hello World");
         System.out.println("Hello again"); 
 	System.out.println("for auto build. hello :)");       
+       System.out.println("ALP ALP ALPA LPA)");
     }
 }
